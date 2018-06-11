5d751fad6b9661d1779182e43329d5a06e5e84ee
DIVYANSH TRIPATHI <divyansh2998@icloud.com>
2018-06-11 18:50:12 +0530

    Readme added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const sha = require("sha1");
 
-var statusReport = require("./rssAmazonConfig.json");
+var statusReport = require("./rssConfig.json");
 
 app.get("/check/hash", (req, res) => {
   res.send(sha(JSON.stringify(statusReport)), 200);
